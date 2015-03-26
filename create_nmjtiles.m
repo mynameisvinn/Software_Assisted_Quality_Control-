775761df329378ecdcc9dffc44098f470344bb55
vincent tang <vin.tang@gmail.com>
2015-03-26 18:24:45 -0400

    segmentation and extraction

---
+++ create_nmjtiles.m
@@ -0,0 +1,89 @@
+
+% function: detect objects
+% inputs:
+% outputs:
+
+%% basic processing
+clc
+
+i1 = imread('red_25.jpg');
+i2 = wiener2(i1, [5 5]);
+
+se = strel('rectangle',[40 40]);
+i3 = imtophat(i2, se);
+
+i4 = imdilate(i3, se);
+
+i5 = imextendedmax(i4,80);
+
+i6 = imclose(i5, se);
+
+i7 = imerode(i6, se);
+
+i8 = imerode(i7, se);
+
+%% generate mask
+
+% http://matlabtricks.com/post-35/a-simple-image-segmentation-example-in-matlab
+
+SE40 = strel('rectangle',[40 40]);
+BW1_1 = imerode(i1,SE40);
+BW1_2 = imdilate(BW1_1,SE40);
+BW1_3 = imdilate(BW1_2,SE40);
+BW1_4 = imdilate(BW1_2,SE40);
+BW_mask = im2bw(BW1_4);
+labels = bwlabel(BW_mask);
+id = labels(6000, 6000);
+man = (labels == id);
+imshow(man)
+
+
+% https://www.mathworks.com/matlabcentral/answers/38547-masking-out-image-area-using-binary-mask
+
+j1 = imread('rgb_25.jpg');
+man = uint8(man);
+j2 = j1.*man;
+
+%% object detection
+
+s = regionprops(i8, 'Centroid', 'BoundingBox', 'Area');
+
+
+%% high level inspection
+
+% show ROI on original image
+image = imread('rgb_25.jpg');
+imshow(image)
+
+% define isinbox lambda function
+isInBox = @(M,B) (M(:,1)>B(1)).*(M(:,1)<B(1)+B(3)).*(M(:,2)>B(2)).*(M(:,2)<B(2)+B(4));
+
+for i = 1:length(s)
+    centers = s(i).Centroid;
+    if (isInBox(centers, rect)) && (s(i).Area < 200)
+        viscircles(centers,100);
+    end
+end
+
+%% cropping function
+
+% cropping and saving ROI tiles
+
+x = []
+y = []
+id = []
+
+for i = 1:length(s)
+    centers = s(i).Centroid;
+    if isInBox(centers, rect)
+        tile = imcrop(image, [centers-100, 512, 512]);
+        filename = strcat(int2str(i),'.jpg')
+        imwrite(tile, filename, 'jpg');
+        id = vertcat(id, i);
+        x = vertcat(x, s(i).Centroid(1));
+        y = vertcat(y, s(i).Centroid(2));
+    end
+end
+
+T = table(id, x, y)
+write(T, 'output.txt')
--- extract_boundary.m
+++ extract_boundary.m
@@ -1,18 +1,22 @@
-i1 = imread('red_channel_25.jpg');
+%% function: script calculates minimum box around largest object. the
+% coordinates of the bounding box should be used to exclude detected
+% objects outside of the tissue.
+% input: jpeg
+% output: array
+% notes: should explore active contours in next iteration
 
+clc
+
+i1 = imread('red_25.jpg');
 bw = im2bw(i1, graythresh(i1));
-bw2 = imfill(bw)
-
-s = regionprops(bw, 'Area', 'PixelList', 'BoundingBox');
+s = regionprops(bw, 'Area', 'BoundingBox');
 
 % find largest object
 [~,ind] = max([s.Area]);
-
-rect = s(ind).BoundingBox
+rect = s(ind).BoundingBox % rect is array containing bounding box vertices
 
 % draw rectangle over object
 imshow(bw)
 rectangle('Position',rect,'EdgeColor','r')
 
 
-% active contours
