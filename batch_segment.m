2c07ce8cd5c7872ea361a51c79a99a7b2457cc75
mynameisvinn <vincent.tang@canrockventures.com>
2015-04-01 20:04:41 +0000

    remove function red jpeg (there is no need for red jpegs)

--- batch_segment.m
+++ batch_segment.m
@@ -1,5 +1,4 @@
-% version 1.0 on 4/1/2015 9:30AM
-% version 1.1 on 4/1/2015 11:30AM
+% v1.2 4pm on 4/1/2015 4pm
 
 % function: when executed from folder containing images, script will
 % convert each image into 3 images: (1) rgb jpeg; (2) red jpeg; (3) cropped
@@ -16,17 +15,14 @@ for i = 3:length(list_of_files)-1
     directoryname = char(new_folder_name(1))
     mkdir(directoryname)
     
-    % convert each JP2 image into rgb jpeg and red jpeg
+    % convert each JP2 image into rgb jpeg
+    % no need for red jpeg since subsetting
     
     i1 = imread(file_name);
     i1 = uint8(i1);
     path = strcat(char(directoryname), char('/'), 'rgb_25.jpg')
     imwrite(i1, path, 'jpg', 'mode', 'lossy', 'Quality', 25);
-
-    i1_red = i1(:,:,1);
-    path = strcat(char(directoryname), char('/'), 'red_25.jpg')
-    imwrite(i1_red, path, 'jpg', 'mode', 'lossy', 'Quality', 25);
-    
+   
     % convert each JP2 image into cropped rgb jpeg via mask
     % http://matlabtricks.com/post-35/a-simple-image-segmentation-example-in-matlab
 
