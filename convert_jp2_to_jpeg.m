0eac2ef384bcac157ec36b510c0d891a1b0a4cc6
mynameisvinn <vincent.tang@canrockventures.com>
2015-04-01 19:11:11 +0000

    include tiff conversion

--- convert_jp2_to_jpeg.m
+++ convert_jp2_to_jpeg.m
@@ -1,19 +1,14 @@
-%% image format conversion
+% function: converts 3-channel, 12 bit JP2 into 8-bit, lossy red and green JPEGS
+% input: JP2 image
+% output: (1) compressed 8 bit red JPEG; (2) compressed 8 bit RGB JPEG; (2)
+% uncompressed 8 bit TIFF
 
-% function: converts 12 bit JP2 RGB into 8-bit, lossy red and green JPEGS
-% input: JP2
-% output: (1) 8 bit red JPEG at 25% compression; (2) 8 bit green JPEG at 25% compression; 
-
-i1 = imread('test.jp2');
+i1 = imread('25_10_2015-02-18_3_1020_()_4_2_18_schema_assigned_134824_lossy.jp2');
 i2 = uint8(i1);
-
-% create red channel
 i2_red = i2(:,:,1);
 
-% create green channel
-i2_green = i2(:,:,2);
-
-% save files to disk
 imwrite(i2, 'rgb_25.jpg', 'jpg', 'mode', 'lossy', 'Quality', 25);
+imwrite(i2, 'rgb_25.tif', 'tiff');
 imwrite(i2_red, 'red_25.jpg', 'jpg', 'mode', 'lossy', 'Quality', 25);
-imwrite(i2_green, 'green_25.jpg', 'jpg', 'mode', 'lossy', 'Quality', 25);
+
+clear
