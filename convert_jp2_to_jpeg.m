b3ea3c68e17c5b88e6c53946c5e5e181e6bbdea7
mynameisvinn <vincent.tang@canrockventures.com>
2015-04-09 18:54:04 +0000

    modified to create tiff files

--- convert_jp2_to_jpeg.m
+++ convert_jp2_to_jpeg.m
@@ -1,14 +1,13 @@
-% function: converts 3-channel, 12 bit JP2 into 8-bit, lossy red and green JPEGS
-% input: JP2 image
-% output: (1) compressed 8 bit red JPEG; (2) compressed 8 bit RGB JPEG; (2)
-% uncompressed 8 bit TIFF
+clc
+tic
+i1 = imread('37_96_2015-03-30_2_1579_()_2_1_50_1_schema_assigned_464807_lossy.jp2');
 
-i1 = imread('25_10_2015-02-18_3_1020_()_4_2_18_schema_assigned_134824_lossy.jp2');
+% convert to 8bit image
+% for better performance, create new variable when changing class type
 i2 = uint8(i1);
-i2_red = i2(:,:,1);
 
-imwrite(i2, 'rgb_25.jpg', 'jpg', 'mode', 'lossy', 'Quality', 25);
-imwrite(i2, 'rgb_25.tif', 'tiff');
-imwrite(i2_red, 'red_25.jpg', 'jpg', 'mode', 'lossy', 'Quality', 25);
+% imwrite(i2, 'rgb.jpg', 'jpg', 'Quality', 100);
 
-clear
+% option: create tiff files
+imwrite(i2, 'rgb.tif', 'tiff');
+toc
