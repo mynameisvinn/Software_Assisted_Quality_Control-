a46c25a8ca507d1ecb273b2da86c20bb8682daa7
vincent tang <vin.tang@gmail.com>
2015-10-19 18:38:25 -0400

    include function for sorting by RS

--- Generate Patches/driver.m
+++ Generate Patches/driver.m
@@ -1,10 +1,17 @@
-for q =1:length(list_of_data_id)
+% @date: 10/19/15
+% @author: vin tang
+% @function: for each datafile ID, generate patches according to raw
+% specimen
+
+for idx = 1:length(list_of_data_id)
     
     tic
-    data_id = list_of_data_id(q);
-    disp(q);
+    raw_specimen = list_of_data_id(idx, 1);
+    data_id = list_of_data_id(idx, 2);
+    
+    disp(idx);
     disp(data_id);
 
-    generate_patch(data_id);
+    generate_patch(data_id, raw_specimen);
     toc
 end
--- Generate Patches/generate_patch.m
+++ Generate Patches/generate_patch.m
@@ -1,41 +1,49 @@
-function [idx] = generate_patch(data_idx)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [idx] = generate_patch(data_idx, raw_specimen)
+% @date: 10/19/15
+% @author: vincent tang
 
-    % retreive image file path
-    % connect to limsdb
+% function: generate patches for user id 8 and counter 1
+
+% parameters: 
+% @data_idx: datafile ID
+% @raw_specimen: corresponding raw specimen folder
+
+    % retreive image
     conn = database('limsdb','lims','mou53Brains!');
     curs = exec(conn,[]);
     sql = ['SELECT primary_file_system_location_lossy FROM data_file WHERE id = "' num2str(data_idx) '"'];
     curs = exec(conn,sql);
     curs = fetch(curs);
     imagefilename = curs.Data;
-
-    % create image file path
     path = strcat('Z:\Converted Image Data\', imagefilename);
+    im_raw = imread(char(path));
+    im_8bit = uint8(im_raw);
 
-
-    im0 = imread(char(path));
-
-    % recast from uint16 to uint8
-    im1 = uint8(im0);
-
-    % query limsdb for actual coordinates
-    sql = ['SELECT click_location_x_coordinate, click_location_y_coordinate FROM limsdb.data_file_click where (user_id = 4) and (counter_id = 1) and data_file_id = "' num2str(data_idx) '"'];
+    % grab coordinates
+    sql = ['SELECT click_location_x_coordinate, click_location_y_coordinate FROM limsdb.data_file_click where (user_id = 8) and (counter_id = 1) and data_file_id = "' num2str(data_idx) '"'];
     curs = exec(conn,sql);
     curs = fetch(curs);
-    tmr_actuals = cell2mat(curs.Data);
+    predictions = cell2mat(curs.Data);
+    
+    % generate patches from coordinates
+    for idx = 1:length(predictions(:,1))
 
-    for idx = 1:length(tmr_actuals)
-
-        a = tmr_actuals(idx,1);
-        b = tmr_actuals(idx,2);
+        a = predictions(idx,1);
+        b = predictions(idx,2);
         patch_size = 50;
 
-        tmr_patch = im1( b-patch_size: b +patch_size, a - patch_size: a + patch_size,:);
-        filename = strcat(int2str(data_idx), '_', int2str(a), '_', int2str(b),'.tif');
-        imwrite(tmr_patch, filename, 'tif');
+        tmr_patch = im_8bit( b-patch_size: b +patch_size, a - patch_size: a + patch_size,:);
         
+        % save patches in the appropriate raw specimen folder, if it exists
+        try
+            filename = strcat(int2str(raw_specimen), '/', int2str(data_idx), '_', int2str(a), '_', int2str(b),'.tif');
+            imwrite(tmr_patch, filename, 'tif');
+            
+        % otherwise create new folder
+        catch
+            mkdir(int2str(raw_specimen));
+            filename = strcat(int2str(raw_specimen), '/', int2str(data_idx), '_', int2str(a), '_', int2str(b),'.tif');
+            imwrite(tmr_patch, filename, 'tif');
     end
 
 end
