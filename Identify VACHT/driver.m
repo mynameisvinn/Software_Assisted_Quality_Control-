fb7da27d996aed28b2cf52061defcdb14f091b32
vincent tang <vin.tang@gmail.com>
2015-10-28 12:34:33 -0400

    include watershed segmentation for vacht analysis

---
+++ Identify VACHT/driver.m
@@ -0,0 +1,25 @@
+list_paths = dir;
+%%
+for i = 3:length(list_paths)
+    tic
+    path = list_paths(i).name;
+   
+       disp(i);
+       disp(path);
+
+       if identify_vacht_2(path, 70, 90) == 1
+            new_path = strcat('true/', path);
+
+       else
+            new_path = strcat('false/', path);
+       end
+
+       im_raw = imread(path);
+       patch = im_raw * 2^6;
+       patch(:,:,1) = 0;
+       patch(:,:,2) = patch(:,:,2) * 2.5;
+       patch(:,:,3) = 0;
+
+       imwrite(patch, new_path)
+       toc
+end
--- Identify VACHT/identify_vacht_2.m
+++ Identify VACHT/identify_vacht_2.m
@@ -1,4 +1,4 @@
-function [class] = identify_vacht(path, min_area, threshold)
+function [class] = identify_vacht_2(path, min_area, threshold)
 
     im_raw = imread(path);
     im_uint8 = uint8(im_raw);
@@ -21,7 +21,7 @@ function [class] = identify_vacht(path, min_area, threshold)
 
     % im_peaks = im_enhanced > prctile(median(im_green), threshold);
     % im_peaks = imextendedmax(im_enhanced, prctile(median(im_green), level * 100));
-    im_peaks = im_enhanced > 250;
+    im_peaks = im_enhanced > prctile(median(im_green), 90);
 
     combined_masked_2 = combined_masked_1 .* im_peaks;
     
@@ -31,7 +31,26 @@ function [class] = identify_vacht(path, min_area, threshold)
     combined_masked_2 = imclose(combined_masked_2, se);
     combined_masked_2 = bwareaopen(combined_masked_2, 2);
     % combined_masked_2 = imclearborder(combined_masked_2);
-    area = sum(sum(combined_masked_2));    
+    
+    
+    patch = im_raw * 2^6;
+    G_kernel = fspecial('gaussian', [3 3], 15); % ([kernel dimensions], sigma); larger sigma values will reduce count
+    patch = patch(50:150,50:150,2);
+    I = imfilter(patch, G_kernel ,'same', 'conv');
+    hy = fspecial('sobel');
+    hx = hy';
+    Iy = imfilter(double(I), hy, 'replicate');
+    Ix = imfilter(double(I), hx, 'replicate');
+    gradmag = sqrt(Ix.^2 + Iy.^2);
+    g = gradmag - min(gradmag(:));
+    g = g / max(g(:));
+    th = graythresh(g); %# Otsu's method.
+    a = imhmax(g,th/2); %# Conservatively remove local maxima.
+    combined_3 = a .* combined_masked_2;
+    combined_4 = combined_3 > .2;
+    
+    
+    area = sum(sum(combined_4));    
 
     % inspect largest object
     if (area > min_area)
